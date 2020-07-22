03b090399087fc19ff3b3d04dab790ef019a41e0
Ritika-Agrawal811 <65513097+Ritika-Agrawal811@users.noreply.github.com>
2020-07-23 00:16:57 +0530

    Add files via upload

---
+++ scripts/slideshow.js
@@ -0,0 +1,27 @@
+
+var slideIndex=1;
+showSlides(slideIndex);
+
+function nextSlide(n){
+  showSlides(slideIndex+=n);
+    console.log(slideIndex);
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName('slides');
+    
+    if(n>slides.length){
+        slideIndex=1;
+    }
+    
+    if(n<1){
+        slideIndex=slides.length;
+    }
+    
+    for(i=0;i<slides.length;i++){
+        slides[i].style.display="none";
+    }
+
+    slides[slideIndex-1].style.display="block";
+}
